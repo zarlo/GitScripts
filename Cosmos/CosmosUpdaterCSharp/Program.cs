91402be9b258f2d1f16ca91abcb54155dca6cf85
zarlo <zarlo5899@gmail.com>
2017-12-04 15:40:20 +1000

    update

---
+++ Cosmos/CosmosUpdaterCSharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CosmosUpdaterCSharp
+{
+    class Program
+    {
+
+
+        static string IL2CPU = "https://github.com/CosmosOS/IL2CPU.git";
+        static string XSharp = "https://github.com/CosmosOS/XSharp.git";
+        static string Cosmos = "https://github.com/CosmosOS/Cosmos.git";
+
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+            }
+
+        }
+
+        static void update(string git, string Path)
+        {
+
+
+            if (System.IO.Directory.Exists(Path))
+            {
+            }
+            else
+            {
+            }
+
+
+
+        }
+
+    }
+}
